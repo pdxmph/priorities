b571a2f45f8ed14cecb848fe7794631ca107c3d8
Mike Hall <mph@puddingbowl.org>
2016-03-05 11:22:20 -0800

    Basic layout and routing changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   resources :goals
   resources :teams
   devise_for :users
+
+  get '/users/' => 'users#index'
+  match '/users/:id', :to => 'users#show', :as => :user,  :via => :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
