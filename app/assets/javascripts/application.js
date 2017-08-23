54cc35f28636172cc9d1baf7257e7a489bd605a1
Mike Hall <mph@puddingbowl.org>
2017-08-23 12:34:22 -0700

    remove highlight effects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require jquery-ui/effect-highlight
 //= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require autocomplete-rails
